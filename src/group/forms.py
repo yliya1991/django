3c68ba050e0fad36abd16086cda98337bbc3823d
yuliya.garagasheva <yuliya.zayceva.911@gmail.com>
2020-05-13 14:47:02 +0300

    new file

---
+++ src/group/forms.py
@@ -0,0 +1,16 @@
+from django import forms
+
+from group.models import Group
+
+
+class GroupCreateForm(forms.ModelForm):
+    class Meta:
+        model = Group
+        fields = (
+            'first_name',
+            'last_name',
+            'group_code',
+            'group_number',
+            'training_completed',
+            'form_of_training',
+        )
